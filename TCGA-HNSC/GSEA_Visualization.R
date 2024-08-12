5d50f097477949b32c12c5bcbd19139117f18434
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2024-08-12 10:44:58 +0200

    Update GSEA_Visualization.R

--- TCGA-HNSC/GSEA_Visualization.R
+++ TCGA-HNSC/GSEA_Visualization.R
@@ -1,10 +1,16 @@
+#############
+# Libraries #
+#############
+
 library(data.table)
 library(fgsea)
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
-#Here I have first run the GSEA analysis with GSEA software. Then from the results folder I have imported the following files
-setwd("C:/Users/abe186/UiT Office 365/O365-Bioinformatikk TRIM27 - General/TCGA-Head and neck/GSEA/gobp_hnsc_tcga_high_vs_low_t32.Gsea.1714027534091")
+
+#####################
+# Read gsea results #
+#####################
 
 high<-as.data.frame(fread("gsea_report_for_High_1714027534091.tsv"))
 low <- as.data.frame(fread("gsea_report_for_Low_1714027534091.tsv"))
@@ -36,6 +42,3 @@ dev.off()
 #To save
 ggexport(plot, filename = "GSEA_NOROC_High_vs_Low_HEV_GOBP.png",res=200,width = 3200, height = 2000)
 ggexport(plot, filename = "GSEA_NOROC_High_vs_Low_HEV_GOBP.pdf",width = 25, height = 15)
-
-?labs
-?stat_summary
