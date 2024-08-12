13da50720e714cd97ff7808834d0276d95bfe065
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2024-08-12 10:09:44 +0200

    Add files via upload

---
+++ KO_HSC3/housekeeping_genes.R
@@ -0,0 +1,92 @@
+library(tidyverse)
+library(readxl)
+library(ComplexHeatmap)
+library(dendsort)
+
+#####################
+## Expression file ##
+#####################
+
+raw <- as.data.frame(read_xlsx("hsc3_t47d_counts.xlsx"))
+
+raw <- distinct(raw, Name, .keep_all = T)
+rownames(raw) <- raw$Name
+raw <- dplyr::select(raw, contains("CPM"))
+
+raw <- dplyr::select(raw, contains("T47D"))
+
+raw <- dplyr::select(raw, contains("HSC3"))
+
+colnames(raw) <- gsub("- CPM", "", colnames(raw))
+#raw <- raw[,-6]
+colnames(raw) <- c("T47D_KO_15", "T47D_KO_21", "T47D_KO_29", "T47D_KO_33", 
+                    "T47D_WT_1", "T47D_WT_2", "T47D_WT_3") 
+
+colnames(raw) <- c("HSC3_KO_1", "HSC3_KO_3", "HSC3_KO_6", "HSC3_WT_1", 
+                    "HSC3_WT_2", "HSC3_WT_3")
+
+raw$Gene <- rownames(raw)
+
+housekeeping <- c("GAPDH", "B2M", "ACTB", "TBP", "RP",
+                  "RRN18S", "DIMT1", "TUBA1A", "GUSB", "PGK1", "RPL13A",
+                  "PUM1", "DHX9", "MZT2B", "UBXN4", "LARP1", "TAF2", "STX5",
+                  "SYMPK", "TMEM11", "SDHA", "TFRC", "HMBS")
+
+hs <- subset(raw, (Gene %in% housekeeping))
+hs$Gene <- NULL
+
+#############
+## Heatmap ##
+#############
+
+h <- as.matrix(log2(1+hs))
+
+h_breaks <- seq(min(h), max(h), length.out = 10)
+h_breaks
+
+
+#Reposition the breaks in quantile positions
+quantile_breaks <- function(xs, n = 10) {
+  breaks <- quantile(xs, probs = seq(0, 1, length.out = n))
+  breaks[!duplicated(breaks)]
+}
+
+h_breaks <- quantile_breaks(h, n = 11)
+h_breaks
+
+row_dist <- as.dist(1-cor(t(h), method = "pearson"))
+row_hc <- hclust(row_dist, method = "complete")
+Rowv=dendsort(as.dendrogram(row_hc), isRevers=TRUE, type = "average") 
+
+library(dendsort)
+library(circlize)
+library(RColorBrewer)
+col_fun <- colorRamp2(quantile_breaks(h, n = 11), c("#1e90ff", "#0479f5", "#0467d2", "#0356af", "#02458c","#023369","#012246", "#130202", "#6b0002","#b20004","#ff0000"))
+
+pdf("Housekeeping_genes_hsc3_wt_vs_ko.pdf",  width=13,height=16)
+ht <- Heatmap(h,col = col_fun,
+              cluster_columns = F,
+              name = "Expression Values",
+              show_heatmap_legend = T ,
+              #top_annotation = ha,
+              #left_annotation = hr,
+              show_column_names = T,
+              show_row_names = T,
+              cluster_rows = Rowv,
+              row_title_gp = gpar(fontsize=12),
+              column_title_gp = gpar(fontsize=12),
+              height = unit(12, "cm"),
+              width = unit(10.55, "cm"),
+              column_dend_reorder = F,
+              row_dend_reorder = T,
+              show_row_dend = T,
+              border = T,
+              column_dend_height = unit(2, "cm"),
+              column_names_rot = 45,
+              #legend_height = unit(4, "cm"),
+              column_names_gp = grid::gpar(fontsize = 7,fontface="bold"),
+              column_split = rep(c("KO", "KO", "KO", "WT", "WT", "WT")),
+              heatmap_legend_param = list(title="Log2(1+CPM)", legend_height=unit(3, "cm"),title_gp=gpar(fontsize=10, fontface="bold"),labels_gp = gpar(fontsize=8)))
+
+draw(ht, merge_legend=TRUE, padding = unit(c(2, 2, 2, 2), "mm"))
+dev.off()
---
+++ KO_HSC3/limma_hsc3.R
@@ -0,0 +1,143 @@
+library(edgeR)
+library(data.table)
+library(tidyverse)
+library(ggpubr)
+library(Glimma)
+library(biomaRt)
+library(Homo.sapiens)
+library(EnhancedVolcano)
+
+#####################
+## Expression file ##
+#####################
+
+expr <- as.data.frame(fread("hsc_wt_ko_counts.csv"))
+expr <- expr[,-c(2:4)]
+expr <- distinct(expr,expr$Name,.keep_all = TRUE)
+expr$`expr$Name` <- NULL
+expr <- column_to_rownames(expr, "Name")
+
+colnames(expr) <- c("HSC3_KO_1", "HSC3_KO_3", "HSC3_KO_6", "HSC3_WT_1", 
+                    "HSC3_WT_2", "HSC3_WT_3") 
+
+##############################
+## Create group information ## 
+##############################
+
+meta <- as.data.frame(fread("metadata_hsc3.csv"))
+meta <- meta[,-1]
+
+#get same patients in expr as the clinical file
+filtered_colnames <- intersect(names(expr), meta$name)
+expr <- expr[, filtered_colnames, drop = FALSE]
+
+
+#Make sure the group info is in the same order as the expr file
+meta <- column_to_rownames(meta, "name")
+meta <- meta %>% arrange(treatment)
+expr<- expr[,rownames(meta)]
+all(rownames(meta) == colnames(expr))
+
+
+meta$sample <- rownames(meta)
+test_meta <- meta[-3,]
+test_meta$lane <- c("3", "4", "4", "4", "4", "1")
+
+treatment <- as.factor(meta$treatment)
+
+test_treat <- as.factor(test_meta$treatment)
+lane <- as.factor(test_meta$lane)
+###########
+## LIMMA ##
+###########
+test <- expr[,-3]
+
+#Create DGEList object
+d0 <- DGEList(expr)
+d0
+
+
+#Filtering
+keep.exprs <- filterByExpr(d0, group= treatment) #By default it filters out genes that has total counts below 10 in the minimum number of samples
+d0 <- d0[keep.exprs,, keep.lib.sizes=FALSE]
+dim(d0)
+
+
+#Normalizing the actual data now and visualizing
+d0 <- calcNormFactors(d0, method = "TMM")
+lcpm <- cpm(d0, log=TRUE)
+
+house <- as.data.frame(lcpm)
+house <- as.data.frame(t(house))
+house <- rownames_to_column(house, "id")
+meta <- rownames_to_column(meta, "id")
+house <- merge(house, meta, by = "id")
+
+
+ggplot(house,aes(x = treatment, y = ACTB)) +
+  geom_boxplot(aes(fill = treatment), alpha = 0.5, outlier.shape = NA)+
+  geom_point() +
+  labs(y = "Expression (cpm)", title = "ACTB") + 
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5, face = "bold"), 
+        axis.text.x = element_text(size = 10), 
+        axis.ticks.x = element_blank(), 
+        axis.title.x = element_text(size = 10), 
+        axis.title.y = element_text(size = 11), 
+        axis.text.y = element_text(size = 10), 
+        panel.background = element_rect(fill = "white", colour = "white"),
+        axis.line = element_line(linewidth = 0.7, linetype = "solid", colour = "black")) +
+  scale_fill_manual(values = c("#333ED4", "red"))+ 
+  geom_label(aes(label=id))
+
+#boxplot(lcpm,col=group)
+col_group = treatment
+myColors <- c("red", "green")
+levels(col_group) <- myColors
+col_group <- as.character(col_group)
+plotMDS(lcpm, col = col_group) 
+legend("topright", col=c("red","green"),pch = c(0,1), legend=c("KO","WT"))
+boxplot(lcpm,col=col_group)
+
+design <- model.matrix(~0+treatment)
+
+
+
+colnames(design) <- gsub("treatment","", colnames(design))
+design
+
+
+v <- voom(d0, design, plot=TRUE)
+vfit <- lmFit(v, design)
+
+
+contr <- makeContrasts(WT - KO, levels = colnames(coef(vfit)))
+contr
+
+
+vfit <- contrasts.fit(vfit, contr)
+efit <- eBayes(vfit)
+plotSA(efit, main="Final model: Mean-variance trend")
+summary(decideTests(efit))
+
+top.table <- topTable(efit, sort.by = "P", n = Inf, coef = 1)
+#fwrite(top.table, "limma_trim32_wt_vs_ko.csv")
+
+
+volcano <- top.table 
+volcano$log10 <- 
+
+v <- EnhancedVolcano(top.table,
+                     lab = rownames(top.table),
+                     labSize = 3.0,
+                     pointSize = 2,
+                     labCol = 'black',
+                     labFace = 'italic',
+                     title = "WT vs KO TRIM32",
+                     x = 'logFC',
+                     y = 'adj.P.Val',
+                     pCutoff = 0.05,
+                     FCcutoff = 0.5,
+                     colAlpha = 0.5)
+-log10(top.table$adj.P.Val)
+
