db5b3b70e246ed2786bab1a5fd64a6070f5db57b
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2025-05-25 13:30:44 +0200

    Add files via upload

--- volcano_filtered_genes.R
+++ volcano_filtered_genes.R
@@ -2,11 +2,12 @@ library(tidyverse)
 library(fgsea)
 library(EnhancedVolcano)
 library(data.table)
-#####################
-## Expression file ##
-#####################
 
-expr <- as.data.frame(fread("2025_03_07_linear_reg_noroc_trim32_high_vs_low.csv"))
+###################
+# Expression file #
+###################
+
+expr <- as.data.frame(fread("limma_trim32_WT_1_3_KO_3_6_v2.csv"))
 
 genes_1 <- as.data.frame(gmtPathways("gene_lists/HALLMARK_MYC_TARGETS_V1.v2023.2.Hs (1).gmt"))
 genes_2 <- as.data.frame(gmtPathways("gene_lists/HALLMARK_MYC_TARGETS_V2.v2023.2.Hs (1).gmt"))
@@ -18,12 +19,17 @@ comb <- rbind(genes_1, genes_2)
 dup <-comb[duplicated(comb$gene),]
 comb <- dplyr::distinct(comb, .keep_all = T)
 
-expr_1 <- subset(top.table, rownames(top.table) %in% comb$gene)
+expr_1 <- subset(expr, rownames(expr) %in% comb$gene)
 
 fwrite(expr, "limma_trim32_WT_1_2_3_KO_1_3_6_myc_2025_02_26.csv", row.names = T)
 
+expr_1 <- fread("limma_trim32_WT_1_2_3_KO_1_3_6_myc_2025_02_26.csv")
+expr_1 <- expr_1[,-1]
+
+test <- as.data.frame(expr_1$logFC>0)
+
 v <- EnhancedVolcano(expr_1,
-                     lab = rownames(expr_1),
+                     lab = expr_1$V1,
                      labSize = 6.0,
                      labCol = 'black',
                      labFace = 'bold',
@@ -32,7 +38,7 @@ v <- EnhancedVolcano(expr_1,
                      y = 'adj.P.Val',
                      pCutoff = 0.05,
                      colAlpha = 0.5,
-                     FCcutoff = 0.5,
+                     FCcutoff = 1,
                      ylim = c(0,3), 
                      legendLabels = c('Not sig.', 'Log (base 2) FC', 'Adjusted p-value',
                                       'Adjusted p-value & Log (base 2) FC'),
@@ -43,11 +49,11 @@ v <- EnhancedVolcano(expr_1,
                        sum(expr_1$adj.P.Val < 0.05 & expr_1$logFC < 0)))
 v
 
-png("2025_03_07_volcano_noroc_tri.png", res = 200, height = 1800, width = 1500)
+png("volcanoplot_trim32_WT_1_3_KO_3_6_myc_2025_02_26.png", res = 200, height = 1800, width = 1500)
 print(v)
 dev.off()
 
-pdf("figures/volcano/volvanoplot_trim32_WT_1_3_KO_3_6_PI3K_AKT_MTOR.pdf", height = 8, width = 8)
+pdf("volcanoplot_trim32_WT_1_3_KO_3_6_myc_2025_02_26.pdf", height = 8, width = 8)
 print(v)
 dev.off()
 
