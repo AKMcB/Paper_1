766564272e1382cbad7383fd3d2bd33fe6c8be88
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2025-05-27 09:48:08 +0200

    Update Pearson_t32.R

--- TCGA-HNSC/Pearson_t32.R
+++ TCGA-HNSC/Pearson_t32.R
@@ -41,16 +41,16 @@ merged <- merge(genes, info, by.x = "id", by.y = "Patient ID")
 
 p1 <- ggscatter(merged, x = "TRIM32", y = "MYC",
                 title = " TRIM32 vs MYC (n = 495)",
-                color = "black", shape = 21, size = 2,fill="red",alpha=0.4, # Points color, shape and size
-                add = "reg.line",  # Add regressin line
-                add.params = list(color = "#D73027", fill = "#D73027"), # Customize reg. line
-                conf.int = TRUE, # Add confidence interval
+                color = "black", shape = 21, size = 2,fill="red",alpha=0.4, 
+                add = "reg.line",  
+                add.params = list(color = "#D73027", fill = "#D73027"), 
+                conf.int = TRUE, 
                 cor.coef = TRUE,
-                cor.coef.size = 5,# Add correlation coefficient. see ?stat_cor
+                cor.coef.size = 5,
                 cor.coeff.args = list(method = "pearson", label.sep = "\n"),
                 xlab = "TRIM32", ylab = "MYC")+ 
   theme(legend.position = "bottom",
-        plot.title = element_text(hjust = 0.5, face ="italic"), #Italic if it is a gene. 
+        plot.title = element_text(hjust = 0.5, face ="italic"),  
         axis.text.x = element_text(size=18), axis.ticks.x=element_blank(), 
         axis.title.x = element_text(size = 18), axis.title.y = element_text(size = 18),
         axis.text.y = element_text(size = 18))
